d9d5495668deffa28e1245e7f3fbf1df7d80bac4
Samuel Cornet <samuel@noukod.com>
2018-09-14 20:00:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,11 @@
 def speak_to_grandma(phrase)
   phrase = "I LOVE YOU GRANDMA!"
   if phrase.upcase
-     return "I LOVE YOU TOO PUMPKIN!" 
-  else
-    return "HUH?! SPEAK UP, SONNY!"
-
-  # if phrase.upcase? 
-  #   return "NO, NOT SINCE 1938!"
-  #  phrase == "I LOVE YOU GRANDMA!"?
-  #   return "I LOVE YOU TOO PUMPKIN!"
+     return "I LOVE YOU TOO PUMPKIN!" : "HUH?! SPEAK UP, SONNY!"
+  phrase.upcase? ?
+    return "NO, NOT SINCE 1938!"
+   phrase == "I LOVE YOU GRANDMA!"?
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 
 
