0cea16aca895048d9e93a54a5837b9771efeba48
Samuel Cornet <samuel@noukod.com>
2018-09-14 19:53:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ def speak_to_grandma(phrase)
   if phrase.upcase? ?
     return "NO, NOT SINCE 1938!"
   end
-  if phrase == "I LOVE YOU GRANDMA!"
+   phrase == "I LOVE YOU GRANDMA!"?
     return "I LOVE YOU TOO PUMPKIN!"
-  end
 end
+
 
 
 # If you shout, she can hear you (or at least she thinks so)
